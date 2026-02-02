f8135bacbcb28a45084218115e419a66c77cc74e
nicorigem <nicolas.assis@origemenergia.com>
2026-02-02 14:33:29 -0300

    tratamento de erros de valores inadequados ou numeros nao negativos

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,6 @@
 def soma(a, b):
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+        raise ValueError("Both arguments must be numbers.")
+    if a < 0 or b < 0:
+        raise ValueError("Both arguments must be non-negative.")
     return a + b
